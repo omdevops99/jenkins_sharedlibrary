e5e9da45900ceea7465430b051c03c7a69b7f0f1
om <omejjigiri99@gmail.com>
2024-10-26 20:54:34 +0530

    c

---
+++ vars/ci.groovy
@@ -0,0 +1,32 @@
+def call() {
+pipeline {
+   agent any 
+   stages {
+
+      stage('compile') {
+         steps {
+            echo 'compile'
+         } 
+      }
+
+      stage('testcases') {
+         steps {
+            echo 'testcases'
+         } 
+      }
+
+      stage('build') {
+         steps {
+            echo 'build'
+         } 
+      }
+
+      stage('Release') {
+         steps {
+            echo 'Release'
+         } 
+      }
+   }
+}
+
+}
