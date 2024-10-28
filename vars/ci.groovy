f40e65230525a58a222abe46be7885ef9fea1356
om <omejjigiri99@gmail.com>
2024-10-28 22:50:15 +0530

    c

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,9 +1,13 @@
 def call() {
     node {
         sh 'env'
+        if(Branch_name == "main") {
         stage('code compile'){}
         stage('code checkout'){}
         stage('code Build'){}
+        }
+        else
+        stage('code checkout'){}
         stage('code test cases'){}
         stage('code integration test cases'){}
     }
